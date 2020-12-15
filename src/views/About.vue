98129ab882d12cd487aba8fd2d91e6715daed60a
nakayama <nakayama@Idris-Pro.local>
2020-12-14 22:13:29 -0300

    cubo changes

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Página em construção.</h1>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="cubo logo" src="../assets/favicon.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// // @ is an alias to /src
+// // import HelloWorld from '@/components/HelloWorld.vue'
+
+// export default {
+//   name: 'Home',
+//   components: {
+//     HelloWorld
+//   }
+// }
+</script>
